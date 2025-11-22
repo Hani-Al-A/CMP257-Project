8e8634f10738f074cd647208e80de7d6bae63487
Hani Al Anani <hani.al.a26@gmail.com>
2025-11-22 14:44:47 +0400

    env variable for password

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/ProductDB";
     private static final String USER = "root";
-    private static final String PASSWORD = "hello";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
